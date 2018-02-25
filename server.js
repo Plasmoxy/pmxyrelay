8e8b3b7e8e2d1dc7d3c877a10abd9f29f138fd66
Plasmoxy <sebohacker@gmail.com>
2018-02-25 16:53:00 +0100

    finally a nice relay together with chat

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 var express = require('express');
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var socketio = require('socket.io');
+
+var ssl_options = {
+  key: fs.readFileSync('ssl/private.key'),
+  cert: fs.readFileSync('ssl/certificate.crt'),
+  ca: fs.readFileSync('ssl/ca_bundle.crt')
+};
 
 app = express();
+redirectApp = express();
 
+// redirect from http
+var redirectServer = http.createServer(redirectApp);
+var server = https.createServer(ssl_options, app);
+var io = socketio.listen(server);
+
+// redirect all http requests to https
+redirectApp.get('*', function (req, res, next) {
+  !req.secure ? res.redirect('https://pmxyrelay.fr.openode.io' + req.url) : next();
+})
+
+// routing
 app.use('/.well-known', express.static(__dirname + '/.well-known', {dotfiles:'allow'}))
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-var server = http.createServer(app);
-server.listen(80, function() {
-  console.log('http listening on port 80')
+app.get('/chat', function(req, res) {
+  res.sendFile(__dirname + '/chat.html')
+})
+
+// socket relay
+io.sockets.on('connection', function(client) {
+  client.on('alpha', function(msg) { // channel alpha
+    io.emit('alpha', msg);
+  })
 });
+
+server.listen(443, function() {
+  console.log('https server listening on port 443')
+})
+redirectServer.listen(80, function() {
+  console.log('redirect http server listening on port 80')
+})
